b902ca18450ade92d2020b37d6072446633e7ff3
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-12 11:18:59 -0300

    Create 2456 - Cartas.c

---
+++ C/2456 - Cartas.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int VerificaC(int *v);
+int VerificaD(int *v);
+
+int main()
+{
+    int i, v[5];
+
+    for(i = 0; i < 5; i++)
+        scanf("%d%*c", &v[i]);
+
+    if(VerificaC(v))
+        printf("C\n");
+
+    else if(VerificaD(v))
+        printf("D\n");
+
+    else
+        printf("N\n");
+
+    return 0;
+}
+
+int VerificaC(int *v)
+
+{
+    int i, flag;
+
+    for(i = 0; i < 4; i++)
+    {
+        if (v[i] <= v[i + 1])
+            flag = 1;
+
+        else
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    return flag;
+}
+
+int VerificaD(int *v)
+{
+    int i, flag;
+
+    for(i = 0; i < 4; i++)
+    {
+        if (v[i] >= v[i + 1])
+            flag = 1;
+
+        else
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    return flag;
+}
