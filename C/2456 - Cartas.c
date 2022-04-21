aa026c51ecf4b8704f1914faf5ca5cc670ac383d
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-21 12:55:01 -0300

    Update 2456 - Cartas.c

--- C/2456 - Cartas.c
+++ C/2456 - Cartas.c
@@ -9,13 +9,13 @@ int main() {
     for(i = 0; i < 5; i++)
         scanf("%d%*c", &v[i]);
 
-    // verifies if the array is crescent and, if true, prints "C"
-    // verifica se o vetor é crescente e, se verdadeiro, imprime "C"
+    // Verifies if the array is crescent and, if true, prints "C".
+    // Verifica se o vetor é crescente e, se verdadeiro, imprime "C".
     if(VerificaC(v))
         printf("C\n");
     
-    // verifies if array is decrescent
-    // verifica se o vetor é decrescente
+    // Verifies if array is decrescent.
+    // Verifica se o vetor é decrescente.
     else if(VerificaD(v))
         printf("D\n");
 
@@ -29,13 +29,13 @@ int VerificaC(int *v) {
     int i, flag;
 
     for(i = 0; i < 4; i++) {
-        // verifies if the current element is less than or equal to the next element and, if true, defines flag as 1
-        // verifica se o elemento atual é menor ou igual ao próximo e, se verdadeiro, define a flag como 1
+        // Verifies if the current element is less than or equal to the next element and, if true, defines flag as 1.
+        // Verifica se o elemento atual é menor ou igual ao próximo e, se verdadeiro, define a flag como 1.
         if (v[i] <= v[i + 1])
             flag = 1;
         
-        // if the next element is bigger than the current one, define flags as 0 and stops the function
-        // se o próximo elemento for maior que o atual, define a flag como 0 e para a função
+        // If the next element is bigger than the current one, define flags as 0 and stops the function.
+        // Se o próximo elemento for maior que o atual, define a flag como 0 e para a função.
         else {
             flag = 0;
             break;
