d18290527e68cf80302f166ca6e6c3de1d32673e
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-21 12:20:39 -0300

    Update 1708 - Volta.c

--- C/1708 - Volta.c
+++ C/1708 - Volta.c
@@ -2,8 +2,7 @@
 
 int calculaVolta(int x, int y);
 
-int main()
-{
+int main() {
 	int x, y, laps;
 
 	scanf("%d%*c%d%*c", &x, &y);
@@ -14,14 +13,14 @@ int main()
 	return 0;
 }
 
-int calculaVolta(int x, int y)
-{
+int calculaVolta(int x, int y) {
+	// acumulates the difference between x and y until this difference, represented by aux, is bigger than x
+	// acumula a diferença entre x e y até que essa diferença, representada por aux, seja maior que x
 	int volta = 1, aux = 0;
 
-	while(aux < x)
-	{
-        volta++;
-        aux += (y - x);
+	while(aux < x) {
+        	volta++;
+        	aux += (y - x);
 	}
 
 	return volta;
