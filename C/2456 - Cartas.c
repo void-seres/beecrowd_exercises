9ced2e380c1efb09b7129aac770623619545f906
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-21 12:31:33 -0300

    Update 2456 - Cartas.c

--- C/2456 - Cartas.c
+++ C/2456 - Cartas.c
@@ -3,16 +3,19 @@
 int VerificaC(int *v);
 int VerificaD(int *v);
 
-int main()
-{
+int main() {
     int i, v[5];
 
     for(i = 0; i < 5; i++)
         scanf("%d%*c", &v[i]);
 
+    // verifies if the array is crescent and, if true, prints "C"
+    // verifica se o vetor é crescente e, se verdadeiro, imprime "C"
     if(VerificaC(v))
         printf("C\n");
-
+    
+    // verifies if array is decrescent
+    // verifica se o vetor é decrescente
     else if(VerificaD(v))
         printf("D\n");
 
@@ -22,17 +25,18 @@ int main()
     return 0;
 }
 
-int VerificaC(int *v)
-{
+int VerificaC(int *v) {
     int i, flag;
 
-    for(i = 0; i < 4; i++)
-    {
+    for(i = 0; i < 4; i++) {
+        // verifies if the current element is less than or equal to the next element and, if true, defines flag as 1
+        // verifica se o elemento atual é menor ou igual ao próximo e, se verdadeiro, define a flag como 1
         if (v[i] <= v[i + 1])
             flag = 1;
-
-        else
-        {
+        
+        // if the next element is bigger than the current one, define flags as 0 and stops the function
+        // se o próximo elemento for maior que o atual, define a flag como 0 e para a função
+        else {
             flag = 0;
             break;
         }
@@ -41,17 +45,14 @@ int VerificaC(int *v)
     return flag;
 }
 
-int VerificaD(int *v)
-{
+int VerificaD(int *v) {
     int i, flag;
 
-    for(i = 0; i < 4; i++)
-    {
+    for(i = 0; i < 4; i++) {
         if (v[i] >= v[i + 1])
             flag = 1;
 
-        else
-        {
+        else {
             flag = 0;
             break;
         }
