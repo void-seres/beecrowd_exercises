5815e53182f4399599d2c6e693d3c6dd5d8dd379
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-21 15:50:15 -0300

    Create Ex1016Distancia.java

---
+++ Java/Ex1016Distancia.java
@@ -0,0 +1,16 @@
+package com.void_seres_.;
+
+import java.util.Scanner;
+
+public class Ex1016Distancia {
+    public static void main(String[] args) {
+        int y;
+        Scanner s = new Scanner(System.in);
+
+        y = s.nextInt();
+        // se o carro y se distanica 1 quilômetro a cada 2 minutos do outro carro, só é necessário que multiplique y por 2
+        // if car y take the distance of 1 kilometer each 2 minutes from the other car, it is only needed to multiply y by 2
+
+        System.out.println((y*2) + " minutos");
+    }
+}
