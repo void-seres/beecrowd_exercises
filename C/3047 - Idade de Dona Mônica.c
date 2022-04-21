5a51490331e8c0578fcf6119d9d1b84e52adc8c7
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-21 13:03:31 -0300

    Update 3047 - Idade de Dona Mônica.c

--- C/3047 - Idade de Dona Mônica.c
+++ C/3047 - Idade de Dona Mônica.c
@@ -2,8 +2,7 @@
 
 void imprimeMaior(int x, int y, int z);
 
-int main()
-{
+int main() {
 	int m, a, b, c;
 
 	scanf("%d%*c%d%*c%d%*c", &m, &a, &b);
@@ -14,14 +13,15 @@ int main()
 	return 0;
 }
 
-void imprimeMaior(int x, int y, int z)
-{
+void imprimeMaior(int x, int y, int z){
+	// Compara se o filho A é o mais velho.
 	if(x > y && x > z)
-        printf("%d\n", x);
+		printf("%d\n", x);
+	
+	// Verifica se o filho B é o mais velho.
+	else if (y > x && y > z)
+		printf("%d\n", y);
 
-    else if (y > x && y > z)
-        printf("%d\n", y);
-
-    else
-        printf("%d\n", z);
+	else
+		printf("%d\n", z);
 }
