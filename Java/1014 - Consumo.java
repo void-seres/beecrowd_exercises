90d7164388ff95dc4cdfcc2877f3159e56dc2427
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-21 15:47:06 -0300

    Create 1014 - Consumo.java

---
+++ Java/1014 - Consumo.java
@@ -0,0 +1,17 @@
+package com.void_seres_;
+
+import java.util.Scanner;
+
+public class Consumo {
+    public static void main(String[] args) {
+        int x;
+        double y, gas;
+        Scanner s = new Scanner(System.in);
+
+        x = s.nextInt();
+        y = s.nextDouble();
+        gas = (Math.round((x*1000.0)/y)/1000.0);
+
+        System.out.println(gas + " km/l");
+    }
+}
