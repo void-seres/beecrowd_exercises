66876a74cf0f837cac926e5be315a955945e74ea
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-22 08:55:39 -0300

    Update Schema and Table for 2604-2605.sql

--- PostgreSQL/Schema and Table for 2604-2605.sql
+++ PostgreSQL/Schema and Table for 2604-2605.sql
@@ -1,4 +1,5 @@
---Creating schema and table for 2604
+-- Creating schema and table for problem 2604
+-- Criando esquema e tabela para o problema 2604
 CREATE TABLE products (
   id      INT PRIMARY KEY,
   name    VARCHAR (100),
@@ -15,7 +16,8 @@ VALUES
   (5, 'Chair',              3000,   210.64),
   (6, 'Single bed',         750,    99);
   
--- Creating schemas and tables for 2605
+-- Creating schemas and tables for problem 2605
+-- Criando esquemas e tabelas para o problema 2605
 CREATE TABLE providers (
   id      INT PRIMARY KEY,
   name    VARCHAR(100),
@@ -57,3 +59,27 @@ ADD COLUMN id_providers INT,
 ADD COLUMN id_categories INT,
 ADD FOREIGN KEY (id_providers) REFERENCES providers(id),
 ADD FOREIGN KEY (id_categories) REFERENCES categories(id);
+
+UPDATE products
+SET id_providers = 6, id_categories = 8
+WHERE id = 1;
+
+UPDATE products
+SET id_providers = 1, id_categories = 9
+WHERE id = 2;
+
+UPDATE products
+SET  id_providers = 5, id_categories = 1
+where id = 3;
+
+UPDATE products
+SET  id_providers = 4, id_categories = 6
+where id = 4;
+
+UPDATE products
+SET  id_providers = 3, id_categories = 6
+where id = 5;
+
+UPDATE products
+SET  id_providers = 1, id_categories = 2
+where id = 6;
