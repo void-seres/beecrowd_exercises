a7a63ecfe9ac0afc92701e3593fa7e184596b926
Eduarda Ferreira Costa <77804572+void-seres@users.noreply.github.com>
2022-04-26 15:38:03 -0300

    Update 2396 - Corrida.c

--- C/2396 - Corrida.c
+++ C/2396 - Corrida.c
@@ -1,3 +1,15 @@
+/*Firstly, when this program was written, I didn't know how to use structs.
+So my best solution at the moment to solve it was to use two arrays: one
+storing the cars' initial position, and the other storing the amount of
+time taken to complete each lap. This program will be written in Java or
+Python, with a better approach.*/
+
+/*Primeiramente, quando esse programa foi escrito, eu não sabia usar structs.
+Minha melhor solução naquele momento para resolver este problema foi usar dois
+vetores: um armazenando a posição inicial dos carros, e outra armazenando o
+tempo total que cada um levou para completar a volta. Este programa será escrito
+em Java ou Python com uma abordagem melhor.*/
+
 #include <stdio.h>
 
 int main()
